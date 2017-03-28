85ea9886041e544eee0b8ab74a8d571ec81402f8
liuhang <liuhang93@foxmail.com>
2017-03-28 14:59:40 +0800

    Site updated: 2017-03-28 14:59:39

--- updateHosts.sh
+++ updateHosts.sh
@@ -2,13 +2,13 @@
 # Program:
 #   update the host from a github project!
 root=$(pwd)
-echo "cd the hosts project"
+echo "cd the hosts project......"
 cd /Users/liuhang/Git/hosts
-echo "start update host by git fethc and git merge"
+echo "start update host by git fethc and git merge......"
 git fetch origin
 git merge origin/master
-echo "copt the host to hexo/source"
+echo "copy the host to hexo/source......"
 cp -f hosts $root/source
-echo "updat hexo..."
+echo "update hexo..."
 cd $root
 hexo g -d
